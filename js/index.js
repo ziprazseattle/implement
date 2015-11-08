0ad977d559bdc975a7499d390a2014dc8717d16a
ziprazseattle <ziprazseattle@yahoo.com>
2015-11-08 13:00:50 -0800

    Framework and some questions/comments

---
+++ js/index.js
@@ -0,0 +1,26 @@
+
+$(document).ready(function() {
+
+/* js is connected 
+function myFunction() {
+    alert("Hello!");
+}
+myFunction();
+*/
+
+
+//** index.html
+//username email format only
+//upon submission/enter key of user name: Save to database, open password window above page
+
+//window above index.html
+//once pw is entered, go to Step Up dashboard page
+
+
+
+//** step-up-dashboard
+//flyout dashboard window on right
+
+
+//end doc ready 
+}); 
