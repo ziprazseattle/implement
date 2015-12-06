a937ffb9008e99138c3030ff20da7521bd9f12d2
ziprazseattle <ziprazseattle@yahoo.com>
2015-12-05 16:46:56 -0800

    Pie jquery
    Can I change image to third options on second click?

--- js/index.js
+++ js/index.js
@@ -9,44 +9,47 @@ myFunction();
 */
 
 
-/*** index.html ***/  
+//.addClass()
 
-//username email format only
-//upon submission/enter key of open password window above page
+	$("#prep").click(advance);
 
-//window above index.html
-//once pw is entered, go to Step Up dashboard page
+	function advance(){ 
+		$("li#prep").css("color", "purple");
+		$("li#prep").css("list-style-image", "url(images/icons/pie-left-bottom.png)")
+		}
 
 
+	
 
-/*** step-up-dashboard ***/
 
 //flyout dashboard window on right
 
-
-$("#checklist-banner").mouseenter(goOrange);
-
-	function goOrange(){
-		$("#checklist-banner").css("background-color", "orange");
-}
-
-$("#checklist-banner").mouseleave(goGrey);
+	$("#checklist-banner").mouseenter(goOrange);
+	$("#checklist-banner").mouseleave(goGrey);
 
 	function goGrey(){
-		$("#checklist-banner").css("background-color", "#888");
-}
+			$("#checklist-banner").css("background-color", "#888");
+		}
+	function goOrange(){
+			$("#checklist-banner").css("background-color", "orange");
+		}
 
 
 
-$("#checklist-banner").click(showList); 
+	$("#checklist-banner").click(showList); 
 
-	function showList(){
-		$("#right-column-layer1").fadeToggle("slow","linear");
-		$("#right-column-flyout").fadeToggle("slow","linear");
-}
+		function showList(){
+			$("#right-column-layer1").fadeToggle("fast","linear");
+			$("#right-column-flyout").fadeToggle("fast","linear");
+		}
+
+
+	$("#slider").slick();
 
 
 /*
+
+
 $("#checklist-banner").click(titleList); 
 
 	function titleList(){
@@ -62,7 +65,6 @@ $("#checklist-banner").click(changeHTML);
 		$("#checklist-banner-a").html("My List &#8675;");
 	}
 
-/*
 
 $("#checklist-banner:last").click(showContent); 
 	function showContent(){
